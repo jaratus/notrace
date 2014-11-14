68c4ee8463fd172c6ec832b7e87deb21b61ad348
rhchen <ranhow.chen@gmail.com>
2014-11-15 00:59:30 +0800

    Add testcase fragement plugin
    	new file:   notrace/net.sf.notrace.atrace.test/.classpath
    	new file:   notrace/net.sf.notrace.atrace.test/.project
    	new file:   notrace/net.sf.notrace.atrace.test/.settings/org.eclipse.jdt.core.prefs
    	new file:   notrace/net.sf.notrace.atrace.test/.settings/org.eclipse.m2e.core.prefs
    	new file:   notrace/net.sf.notrace.atrace.test/META-INF/MANIFEST.MF
    	new file:   notrace/net.sf.notrace.atrace.test/build.properties
    	new file:   notrace/net.sf.notrace.atrace.test/pom.xml
    	new file:   notrace/net.sf.notrace.atrace.test/src/net/sf/notrace/atrace/test/DummyTest.java
    	modified:   notrace/net.sf.notrace.parent/pom.xml

---
+++ notrace/net.sf.notrace.atrace.test/src/net/sf/notrace/atrace/test/DummyTest.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package net.sf.notrace.atrace.test;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+import net.sf.notrace.atrace.AtraceModule;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author admin
+ *
+ */
+public class DummyTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link net.sf.notrace.atrace.AtraceModule#getModuleName()}.
+	 */
+	@Test
+	public void testGetModuleName() {
+		assertThat(new AtraceModule().getModuleName(), CoreMatchers.containsString("Atrace Module"));
+	}
+
+	/**
+	 * Test method for {@link net.sf.notrace.atrace.AtraceModule#version()}.
+	 */
+	@Test
+	public void testVersion() {
+		assertEquals(new AtraceModule().version().getMajorVersion(), 1);
+	}
+
+}
